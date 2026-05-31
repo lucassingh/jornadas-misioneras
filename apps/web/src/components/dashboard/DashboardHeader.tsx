8786b787c967e039c710f65b9c15614fac1e1137
lucas.ped.design1984 <lucas.singh10.@gmail.com>
2026-05-31 19:55:35 -0300

    fix env

--- apps/web/src/components/dashboard/DashboardHeader.tsx
+++ apps/web/src/components/dashboard/DashboardHeader.tsx
@@ -25,7 +25,7 @@ function getInitials(name: string) {
     .split(' ')
     .filter(Boolean)
     .slice(0, 2)
-    .map((part) => part[0].toUpperCase())
+    .map((part) => part[0]!.toUpperCase())
     .join('');
 }
 
