1643dacb9284aaa06d18adebd358e1cbbd0ccc1f
lucas.ped.design1984 <lucas.singh10.@gmail.com>
2026-06-05 11:30:15 -0300

    cambios config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@jornadas/ui', '@jornadas/types'],
+  transpilePackages: ['@jornadas/ui', '@jornadas/types', '@jornadas/database'],
   experimental: {
     optimizePackageImports: ['@mui/material', '@mui/icons-material'],
   },
